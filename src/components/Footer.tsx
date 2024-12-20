2f6de3c8f2ce2fc48c8c66040f799a9a180cbd20
Muhammad Muhammad <muhammadhaidarmuhammad@gmail.com>
2024-12-20 12:46:16 +0300

    modify the footer for better experiance

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,27 @@
 import { FC } from "react";
-import { Link } from "react-router-dom";
 
-const Footer: FC = () => (
-  <div className="bg-blue-500 text-white p-4 text-center mt-auto">
-    <div>&copy; Copyright | All Rights Reserved</div>
-    <div>
-      <Link
-        to="https://alim1496.github.io/"
-        className="hover:underline hover:font-bold opacity-85 hover:opacity-100"
-      >
-        M A Alim
-      </Link>
+const Footer: FC = () => {
+  const getFormattedDate = () => {
+    const date = new Date();
+    const options: Intl.DateTimeFormatOptions = { month: 'long', day: 'numeric' };
+    const daySuffix = (day: number) => {
+      if (day >= 11 && day <= 13) return "th";
+      switch (day % 10) {
+        case 1: return "st";
+        case 2: return "nd";
+        case 3: return "rd";
+        default: return "th";
+      }
+    };
+    const day = date.getDate();
+    return `${date.toLocaleDateString('en-US', options)}${daySuffix(day)}`;
+  };
+
+  return (
+    <div className="bg-blue-500 text-white p-4 text-center mt-auto">
+      <div> {getFormattedDate()} &copy; Copyright | All Rights Reserved for Muhammad Muhammad</div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Footer;
